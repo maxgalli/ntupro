8dc5320cb03e192ab1df0b119f2908a05998833f
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2020-02-18 12:03:59 +0100

    [Run] Introduce multiprocessing with Python module

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from multiprocessing import Pool
+
 from .utils import Count
 from .utils import Histogram
 from .utils import RDataFrameCutWeight
@@ -19,13 +21,18 @@ class RunManager:
     following:
         Dataset()     -->   RDataFrame()
         Selection()   -->   Filter()
-        Count()   -->   Sum()
+        Count()       -->   Sum()
         Histogram()   -->   Histo1D()
 
     Args:
         graphs (list): List of Graph objects that are converted
             node by node to RDataFrame operations
-        parallelize (Bool): Value indicating if the RDataFrame
+        multiprocess (bool): Value indicating if the histogram
+            production is run with the help of the multiprocessing
+            Python package
+        workers (int): number of slaves passed to the
+            multiprocessing.Pool() function
+        parallelize (bool): Value indicating if the RDataFrame
             ImplicitMT is activated
         nthreads (int): number of threads passed to the
             EnableImplicitMT function
@@ -39,21 +46,53 @@ class RunManager:
         friend_tchains (list): List of friend TChains created,
             saved as attribute for the class in otder to not let
             them out of scope
+        multiprocess (bool): Value indicating if the histogram
+            production is run with the help of the multiprocessing
+            Python package
+        workers (int): number of slaves passed to the
+            multiprocessing.Pool() function
         parallelize (Bool): Value indicating if the RDataFrame
             ImplicitMT is activated
         nthreads (int): number of threads passed to the
             EnableImplicitMT function
     """
     def __init__(self, graphs,
+            multiprocess = False,
+            workers = None,
             parallelize = False,
             nthreads = 0):
         self.final_ptrs = list()
         self.tchains = list()
         self.friend_tchains = list()
+        self.multiprocess = multiprocess
         self.parallelize = parallelize
         self.nthreads = nthreads
-        for graph in graphs:
-            self.__node_to_root(graph)
+        if self.multiprocess:
+            if workers is None or workers > len(graphs):
+                workers = len(graphs)
+            for w in range(0, workers):
+                self.final_ptrs.append([])
+                for index in range(w, len(graphs), workers):
+                    self.final_ptrs[w].append(graphs[index])
+            pool = Pool(workers)
+            self.final_ptrs = list(pool.map(self.run_multiprocess, self.final_ptrs))
+            self.final_ptrs = [j for i in self.final_ptrs for j in i]
+        else:
+            logger.debug('%%%%%%%%%% Running Graph-RDF conversion with multiprocessing disabled')
+            for graph in graphs:
+                self.final_ptrs += self.node_to_root(graph)
+
+    def run_multiprocess(self, graph_subset):
+        ptrs = list()
+        for graph in graph_subset:
+            ptrs += self.node_to_root(graph)
+        logger.debug('%%%%%%%%%% Ready to produce a subset of {} shapes'.format(
+            len(ptrs)))
+        results = list()
+        for ptr in ptrs:
+            th = ptr.GetValue()
+            results.append(th)
+        return results
 
     def run_locally(self, of_name, update = False):
         """Save to file the histograms booked.
@@ -62,8 +101,12 @@ class RunManager:
             of_name (str): Name of the output .root
                 file
         """
-        logger.info('%%%%%%%%%% Start producing {} shapes'.format(len(self.final_ptrs)))
-        logger.debug('%%%%%%%%%% Start producing {} shapes'.format(len(self.final_ptrs)))
+        if self.multiprocess:
+            logger.info('%%%%%%%%%% Writing {} shapes to {}'.format(
+                len(self.final_ptrs), of_name))
+        else:
+            logger.info('%%%%%%%%%% Start producing {} shapes'.format(
+                len(self.final_ptrs)))
         if update:
             root_file = TFile(of_name, 'UPDATE')
         else:
@@ -72,20 +115,22 @@ class RunManager:
             op.Write()
         root_file.Close()
 
-    def __node_to_root(self, node, rcw = None):
+    def node_to_root(self, node, final_ptrs = None, rcw = None):
+        if final_ptrs is None:
+            final_ptrs = list()
         if node.kind == 'dataset':
-            logger.debug('%%%%%%%%%% __node_to_root, converting to ROOT language the following dataset node\n{}'.format(
+            logger.debug('%%%%%%%%%% node_to_root, converting to ROOT language the following dataset node\n{}'.format(
                 node))
             result = self.__rdf_from_dataset(
                 node.unit_block)
         elif node.kind == 'selection':
             if len(node.children) > 1:
-                logger.debug('%%%%%%%%%% __node_to_root, converting to ROOT language the following crossroad node\n{}'.format(
+                logger.debug('%%%%%%%%%% node_to_root, converting to ROOT language the following crossroad node\n{}'.format(
                     node))
             result = self.__cuts_and_weights_from_selection(
                 rcw, node.unit_block)
         elif node.kind == 'action':
-            logger.debug('%%%%%%%%%% __node_to_root, converting to ROOT language the following action node\n{}'.format(
+            logger.debug('%%%%%%%%%% node_to_root, converting to ROOT language the following action node\n{}'.format(
                 node))
             if isinstance(node.unit_block, Count):
                 result = self.__sum_from_count(
@@ -95,9 +140,10 @@ class RunManager:
                     rcw, node.unit_block)
         if node.children:
             for child in node.children:
-                self.__node_to_root(child, result)
+                self.node_to_root(child, final_ptrs, result)
         else:
-            self.final_ptrs.append(result)
+            final_ptrs.append(result)
+        return final_ptrs
 
     def __rdf_from_dataset(self, dataset):
         t_names = [ntuple.directory for ntuple in \
@@ -160,7 +206,6 @@ class RunManager:
         l_edges = vector['double']()
         for edge in edges:
             l_edges.push_back(edge)
-
 
         if not weight_expression:
             logger.debug('%%%%%%%%%% Attaching histogram called {}'.format(name))
